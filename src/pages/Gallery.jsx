cf9fe42f0372f3058457a807a7b906776d316c99
dailydoseofseth <dailydoseofseth@gmail.com>
2026-04-08 14:22:38 -0600

    moved JSX elem (img) to Gallery & ADD textALign:Center

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -3,6 +3,8 @@ export default function Gallery() {
   return (
     <>
       <h1>Behold! the Glory of the Gallery Page</h1>
+      <hr />
+      <hr />
       <div className="hero-h1">
         <img
           src="https://miro.medium.com/v2/resize:fit:1400/format:webp/0*YeaUsQyhXSL1TCTH.png"
